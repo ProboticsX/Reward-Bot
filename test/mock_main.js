4aeb636bef9c8c7c8a9794c971e4b8cf5f37fca3
Shivani Shinde <sshinde3@ncsu.edu>
2022-03-08 12:47:14 -0500

    Added mocking tests

---
+++ test/mock_main.js
@@ -0,0 +1,51 @@
+const _ = require("underscore");
+const github = require("./github.js");
+
+async function findCommitPoints(username) {
+	let userdetails = await github.getServerMemberDetails();
+	userdetails = userdetails[0];
+	for (var user in userdetails) {
+		if(user == username) 
+			return userdetails[user].commit;
+	}
+	return 0;
+}
+
+async function findIssuePoints(username) {
+	let userdetails = await github.getServerMemberDetails();
+	userdetails = userdetails[0];
+	for (var user in userdetails) {
+		if(user == username) 
+			return userdetails[user].issue;
+	}
+	return 0;
+}
+
+async function findTotalPoints(username) {
+	let userdetails = await github.getServerMemberDetails();
+	userdetails = userdetails[0];
+	for (var user in userdetails) {
+		if(user == username) 
+			return userdetails[user].total;
+	}
+	return 0;
+}
+
+async function findPRPoints(username, channel) {
+	let userdetails = await github.getServerMemberDetails();
+	userdetails = userdetails[0];
+	for (var user in userdetails) {
+		if(user == username) {
+			for(var c in userdetails[user].pr) {
+				return userdetails[user].pr[c]
+			}
+		}
+	}
+	return 0;
+}
+
+exports.findMostFrequentAssignee = findMostFrequentAssignee;
+exports.findCommitPoints = findCommitPoints;
+exports.findIssuePoints = findIssuePoints;
+exports.findPRPoints = findPRPoints;
+exports.findTotalPoints = findTotalPoints;
