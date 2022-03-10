89cbbf1db32080e95dbc9b83520e868b0b72c2cf
Shruti Satish Magai <smagai@ncsu.edu>
2022-03-10 16:43:44 -0500

    Test cases for positive message analysis

--- test/test.js
+++ test/test.js
@@ -6,11 +6,42 @@ var assert = chai.assert,
 process.env.NODE_ENV = 'test'
 var bot = require('../index');
 var dbPath = 'test/test_data.json';
+var Sentiment = require('sentiment');
 
 
 describe("RewardBot Tests", function() {
 
     this.timeout(5000);
+
+    it("ensures that positiveMessageAnalysis() makes an entry when author isn't present in the database", function() {
+        //bot.getServerMembers();
+        message = {"author": {"username": "test_2"}, "content": "Awesome", "channelId": "1234"};
+        //console.log(message.author.username, message.content, message.channelId);
+        let returnValue = bot.positiveMessageAnalysis(message, dbPath);
+        var sentiment = new Sentiment();
+        var result = sentiment.analyze(message.content);
+        //console.log("From test : ", returnValue["points"]);
+        assert(returnValue["points"] === result.score);
+    });
+    
+    it("ensures that positiveMessageAnalysis() returns false on empty input - 1", function() {
+        message = {"author": {"username": "test"}, "content": "", "channelId": "1234"};
+        //console.log(message.author.username, message.content, message.channelId);
+        let returnValue = bot.positiveMessageAnalysis(message, dbPath);
+        assert(returnValue === false);
+    });
+
+    it("ensures that positiveMessageAnalysis() add the points when author is already present in the database", function() {
+            //bot.getServerMembers();
+            message = {"author": {"username": "test"}, "content": "Awesome", "channelId": "1234"};
+            //console.log(message.author.username, message.content, message.channelId);
+            let returnValue = bot.positiveMessageAnalysis(message, dbPath);
+            var sentiment = new Sentiment();
+            var result = sentiment.analyze(message.content);
+            //console.log("From test : ", returnValue["points"]);
+            assert(returnValue["points"] === result.score);
+    });
+
     
 });
 
