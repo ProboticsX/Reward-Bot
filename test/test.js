55783ea80e86238a55adc6bf0a2063f837ca4caa
Shruti Satish Magai <smagai@ncsu.edu>
2022-03-11 16:34:47 -0500

    Added comments for functions and test case

--- test/test.js
+++ test/test.js
@@ -173,61 +173,10 @@ describe("RewardBot Tests", function() {
             assert(returnValue["points"] === result.score);
     });
 
-    
-
-    it("ensures that calculatePoints() returns 10 points for an issue", function() {
-        
-        type = "issue"
-        let returnValue = bot.calculatePoints(type);
-        assert(returnValue === 10);
-
-    });
-
-    it("ensures that calculatePoints() returns 5 points for an commit", function() {
-        
-        type = "commit"
-        let returnValue = bot.calculatePoints(type);
-        assert(returnValue === 5);
-
-    });
-
-    it("ensures that calculatePoints() returns 0 points for neither an commit or commit", function() {
-        
-        type = ""
-        let returnValue = bot.calculatePoints(type);
-        assert(returnValue === 0);
-
-    });
-
- 
-
-    it("ensures that positiveMessageAnalysis() makes an entry when author isn't present in the database", function() {
-        //bot.getServerMembers();
-        message = {"author": {"username": randomName(6)}, "content": "Awesome", "channelId": "1234"};
-        //console.log(message.author.username, message.content, message.channelId);
-        let returnValue = bot.positiveMessageAnalysis(message, dbPath);
-        var sentiment = new Sentiment();
-        var result = sentiment.analyze(message.content);
-        //console.log("From test : ", returnValue["points"]);
-        assert(returnValue["points"] === result.score);
-    });
-    
-    it("ensures that positiveMessageAnalysis() returns false on empty input - 1", function() {
-        message = {"author": {"username": "test"}, "content": "", "channelId": "1234"};
-        //console.log(message.author.username, message.content, message.channelId);
-        let returnValue = bot.positiveMessageAnalysis(message, dbPath);
-        assert(returnValue === false);
-    });
-
-    it("ensures that positiveMessageAnalysis() add the points when author is already present in the database", function() {
-            //bot.getServerMembers();
-            message = {"author": {"username": "test"}, "content": "Awesome", "channelId": "1234"};
-            //console.log(message.author.username, message.content, message.channelId);
-            let returnValue = bot.positiveMessageAnalysis(message, dbPath);
-            var sentiment = new Sentiment();
-            var result = sentiment.analyze(message.content);
-            //console.log("From test : ", returnValue["points"]);
-            assert(returnValue["points"] === result.score);
+    it("Ensures that writeFile() updates the points in database", function() {
+        msg = {"test":{"commit":0,"issue":0,"pr":{"1234":140},"total":140}}
+        var result = bot.writeFile(dbPath, msg)
+        assert(result == true);
     });
 
     
