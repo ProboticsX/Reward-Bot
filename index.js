1c9f038afc20b16c28b4858dac6b6ed39e55c5a1
ProboticsX <jainshubham0403@gmail.com>
2022-03-30 01:19:25 -0400

    Added messageEmbed for Leaderboard #44

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const express = require('express')
 const app = express()
 var pg = require('pg')
 var format = require('pg-format')
-var table = 'reward_bot'
+var table = 'reward'
 var pool = new pg.Pool(config)
 var myClient
 
@@ -78,6 +78,8 @@ async function main()
         } 
         else if(message.content == "?leaderboard") {
             author_obj = await getLeaderboardDetails(message);
+            sendMessageEmbedForLeaderboard(author_obj["author"], author_obj["desc"], author_obj["type"]);
+
         }
         else {
             author_obj = positiveMessageAnalysis(message);
@@ -130,6 +132,20 @@ async function getLeaderboardDetails(message) {
         return_obj["type"] = type
         return return_obj
     }
+}
+
+function sendMessageEmbedForLeaderboard(author, desc, type) {
+    let userId  = serverMembers[author].id;
+    bot.users.fetch(userId, false).then((user) => {
+        const messageEmbed = new MessageEmbed()
+                .setColor('#0099ff')
+                .setTitle('Leaderboard:')
+                .setAuthor({ name: "Leaderboard", iconURL: embedData[type].iconURL })
+                .setDescription(desc)
+                .setThumbnail(embedData[type].thumbnailUrl)
+                .setTimestamp()
+        user.send({ embeds: [messageEmbed] });
+    });
 }
 
 function rewardForGithubActivity(message) {
